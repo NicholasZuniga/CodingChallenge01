e9cc2c7b7662a859d63b98e20fc8aad4009f7132
NicholasZuniga <Nicholas.M.Zuniga@outlook.com>
2020-11-02 15:33:38 -0600

    Finished Challenges

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiples3and5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            for (int i = 0; i < 1000; i++)
+            {
+                if ((i % 3 == 0) || (i % 5 == 0))
+                {
+                    x = x + i;
+                }
+            }
+            Console.WriteLine("The Sum of of all the multiples of 3 and 5 below 100 is "+ x);
+
+        }
+    }
+}
